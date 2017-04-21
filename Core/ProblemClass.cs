34235b5d0abdf8ba161bab743f525747525576ec
Michele Samorani <msamorani@scu.edu>
2017-04-21 10:09:43 -0700

    commi3

---
+++ Core/ProblemClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    /// <summary>
+    /// A class of problem is a type of problem, such as KP-01, LongestPathProblem, etc
+    /// </summary>
+    public class ProblemClass
+    {
+        public string Name { get; private set; }
+        public ProblemClass(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Core/ProblemInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    /// <summary>
+    /// Represents a problem instance of a certain class of problems
+    /// </summary>
+    public class ProblemInstance <T> where T:ProblemClass
+    {
+        
+    }
+}
