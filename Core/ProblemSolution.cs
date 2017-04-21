6818f258b799164c70c8c2855e4e03beab5953ef
Michele Samorani <msamorani@scu.edu>
2017-04-21 11:07:25 -0700

    withoutClass

---
+++ Core/ProblemSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    /// <summary>
+    /// Class ProblemSolution. It may be a partial or complete solution to a problem instance of a given problem class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class ProblemSolution<T> where T : ProblemClass
+    {
+        public ProblemInstance<T> Instance { get; private set; }
+
+        public ProblemSolution(ProblemInstance<T> instance)
+        {
+            Instance = instance;
+        }
+
+        /// <summary>
+        /// Gets the feasible options of the current solution for the current problem instance
+        /// </summary>
+        /// <returns>IEnumerable&lt;Option&lt;T&gt;&gt;.</returns>
+        public abstract IEnumerable<Option<T>> GetFeasibleOptions();
+
+        /// <summary>
+        /// Gets the attributes of option.
+        /// </summary>
+        /// <param name="o">The o.</param>
+        /// <returns>System.Double[].</returns>
+        public abstract double[] GetAttributesOfOption(Option<T> o);
+    }
+}
